ee37dc1b421be892a960638877691e634b2797ec
Khun Htetz Naing <khunhtetznaing@gmail.com>
2019-03-03 18:02:06 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,92 +1,33 @@
 <?php
-
 $url = $_GET['url'];
-
-$html = get_lol($url);
-
-
-
-$dom = new DOMDocument;
-
-$dom->loadHTML($html);
-
-$data = $dom->getElementsByTagName("script");
-
-$result = "Result";
-
-foreach ($data as $data2) {
-
-    $lol = $data2->nodeValue;
-
-    if (strpos($lol, 'window._sharedData') !== false) {
-
-	$result = $lol;
-$result = str_replace('window._sharedData = ', '', $result);
-
-$result = str_replace(';', '', $result);
-
-	break;
-
+$getSource = file_get_contents($url);
+$deSource = rawurldecode($getSource);
+$data = explode('window._sharedData = ', $deSource)[1];
+$data = explode(';</script>', $data)[0];
+$data = json_decode($data, true);
+$data = $data['entry_data']['PostPage']['0']['graphql']['shortcode_media'];
+$temp = array();
+if(isset($data['edge_sidecar_to_children'])){
+    $tmp = $data['edge_sidecar_to_children']['edges'];
+    foreach ($tmp as $key => $value) {
+        $type = $value['node']['is_video'];
+        $temp['video'] = $type;
+        if($type==true){
+            $temp['url'] = $value['node']['video_url'];
+        }else{
+            $temp['url'] = $value['node']['display_url'];
+        }
+        $output[] = $temp;
+    }
+}else{
+    $type = $data['is_video'];
+    $temp['video'] = $type;
+    if($type==true){
+        $temp['url'] = $data['video_url'];
+    }else{
+        $temp['url'] = $data['display_url'];
+    }
+    $output[] = $temp;
 }
-
-}
-
-$json_a = json_decode($result, true);
-
-$mUrl = array();
-
-$mUrl [] = "url:". '' . $json_a['entry_data']['PostPage']['0']['graphql']['shortcode_media']['display_url'];
-
-
-
-try {
-
-$tmp = $json_a['entry_data']['PostPage']['0']['graphql']['shortcode_media']['edge_sidecar_to_children']['edges'];
-
-if($tmp!=''){
-
-  $mUrl = array();
-
-  foreach ($tmp as $key => $value) {
-
-  $mUrl [] = "url:". '' .($value['node']['display_url']);
-
-  }
-
-}
-
-}catch(Exception $e) {
-
-
-
-}
-
-
-
-$myObj->url=$mUrl;
-
-$myJSON = json_encode($myObj);
-
-echo $myJSON;
-
-
-
-function get_lol($url){
-
-	$cSession = curl_init();
-
-	  curl_setopt($cSession, CURLOPT_URL, $url);
-
-	  curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);	  
-
-	  curl_setopt($cSession, CURLOPT_SSL_VERIFYPEER, false);  
-
-	  $buffer = curl_exec($cSession);
-
-	  curl_close($cSession);
-
-	  return $buffer ;
-
-	}		
-
+echo json_encode($output);
 ?>
